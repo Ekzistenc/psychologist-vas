997dad74d20b89a103902e2b60c39aa4492d82b5
Pavel <your@email.com>
2026-08-26 20:03:08 +0300

    final 2

--- wp-content/plugins/psychologist-vas-blocks/build/header/index.asset.php
+++ wp-content/plugins/psychologist-vas-blocks/build/header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data'), 'version' => '757fd1d9315c1ef7f5c2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data'), 'version' => '81e5d29044876f4f02b3');
